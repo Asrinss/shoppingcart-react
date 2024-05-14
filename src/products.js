b7d53e1b1c8151229775682209839a9a31eeba78
time-tracking-dashboard <a.halic@hotmail.com>
2024-05-14 22:40:47 +0300

    first commit

---
+++ src/products.js
@@ -0,0 +1,94 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.jpg";
+import product9 from "./assets/products/9.jpg";
+import product10 from "./assets/products/10.jpg";
+import product11 from "./assets/products/11.jpg";
+import product12 from "./assets/products/12.jpg";
+import product13 from "./assets/products/13.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 1200,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Air",
+    price: 2300,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon Camera",
+    price: 720,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Samsung Tv",
+    price: 550,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "PS5",
+    price: 480,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Nintendo",
+    price: 250,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "PS4",
+    price: 320,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "AirPods Pro",
+    price: 182,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Vr",
+    price: 650,
+    productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "Samsung Galaxy",
+    price: 980,
+    productImage: product10,
+  },
+  {
+    id: 11,
+    productName: "Apple Watch",
+    price: 698,
+    productImage: product11,
+  },
+  {
+    id: 12,
+    productName: "Samsung Watch",
+    price: 555,
+    productImage: product12,
+  },
+  {
+    id: 13,
+    productName: "Monitor",
+    price: 420,
+    productImage: product13,
+  },
+];
